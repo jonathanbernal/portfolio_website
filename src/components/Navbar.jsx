dbb3fda1d829b8914fed6dc18d3863313e3752e5
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-05-30 22:19:39 -0400

    Rebuild project structure to use React and Vite

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>Jonathan Bernal</h1>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>
+          <a href="https://github.com/jonathanbernal" target="_blank" rel="noreferrer">Github</a>
+        </li>
+        <li><a href="https://linkedin.com/jonathanbernal2" target="_blank" rel="noreferrer">LinkedIn</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
