83fbd4dad6160baffca671470f1d17ed4b0dda35
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-06-01 21:00:36 -0400

    Fix navbar layout and include FA icons.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
+// Import font-awesome icons used on the navbar
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul>
+    <nav className="nav">
+      <ul className="nav--links">
         <li><a href="#">Home</a></li>
-        <li>About</li>
-        <li>Projects</li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
         <li>
-          <a href="https://github.com/jonathanbernal" target="_blank" rel="noreferrer">Github</a>
+          <a href="https://github.com/jonathanbernal" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithub} className="nav--link_icon" />
+          </a>
         </li>
-        <li><a href="https://linkedin.com/jonathanbernal2" target="_blank" rel="noreferrer">LinkedIn</a></li>
+        <li>
+          <a href="https://linkedin.com/jonathanbernal2" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} className="nav--link_icon linkedin_logo" />
+          </a>
+        </li>
       </ul>
     </nav>
   )
 }
+
+
 
 export default Navbar;
