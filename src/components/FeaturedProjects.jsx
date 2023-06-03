933466c12a45be1982b8f9b05ec1ec448ca2df05
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-06-02 23:30:18 -0400

    Add FeaturedProjects component.

---
+++ src/components/FeaturedProjects.jsx
@@ -0,0 +1,12 @@
+import ProjectList from "./ProjectList";
+
+function FeaturedProjects() {
+  return (
+    <div className="featured_projects">
+      <h2 className="featured_projects--title">Featured Projects</h2>
+      <ProjectList />
+    </div>
+  )
+}
+
+export default FeaturedProjects;
