a5ac84aca56b3bf450e719639b89b6604901fb44
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-06-07 23:18:24 -0400

    Update text for featured projects section.

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -4,6 +4,9 @@ function FeaturedProjects() {
   return (
     <div className="featured_projects">
       <h2 className="featured_projects--title">Featured Projects</h2>
+      <p className="featured_projects--description">Here are my most recent projects. For a more comprehensive list, please feel
+        free to head to my <a>Projects</a> section.
+      </p>
       <ProjectList />
     </div>
   )
