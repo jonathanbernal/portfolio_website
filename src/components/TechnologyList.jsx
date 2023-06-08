03445c04b5ad23811ea2dc174038b493c0b9db2e
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-06-07 23:19:36 -0400

    Implement technology list and Project list and CSS

--- src/components/TechnologyList.jsx
+++ src/components/TechnologyList.jsx
@@ -4,8 +4,11 @@ function TechnologyList({ list }) {
   return (
     <div className="technology_list">
       {
-        list.map(iconName => (
-          <IconifyIcon key={iconName} icon={iconName}/>
+        list.map(icon => (
+          <div className="technology_item" key={icon.name}>
+            <IconifyIcon icon={icon.icon}/>
+            <p className="technology_name">{icon.name}</p>
+          </div>
         ))
       }
     </div>
